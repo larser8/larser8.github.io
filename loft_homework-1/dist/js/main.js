9070ac45ecc70d93b26f00335479d7c6d093da91
Denis Beletskiy <denisbeletskiy@Deniss-MacBook-Air.local>
2022-02-04 17:11:36 +0200

    hw3:added burger menu

--- loft_homework-1/dist/js/main.js
+++ loft_homework-1/dist/js/main.js
@@ -1,8 +1,42 @@
 const body = document.body;
 const burger = document.querySelector('.burger__menu');
+const nav = document.querySelector('.list');
 const activeClass = 'is-active';
 
-burger.addEventListener('click', function(e) {
+burger.addEventListener('click', (e) =>{
   e.preventDefault();
+  toggleClass();
+});
+
+nav.addEventListener('click', (e) => {
+  e.preventDefault();
+  if(e.target.classList.contains('list__link')) {
+    toggleClass();
+  }
+});
+
+
+function toggleClass (e) {
   body.classList.contains(activeClass) ? body.classList.remove(activeClass) :body.classList.add(activeClass) 
-})
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const links = document.querySelectorAll('.list__link');
+
+// links.forEach(function(element){
+//   element.addEventListener('click', toggleMenu);
+// })
