9b467c4247f1884b7d3aabaa374698ca993dcc2b
Denis Beletskiy <denisbeletskiy@Deniss-MacBook-Air.local>
2022-02-05 21:10:19 +0200

    hw4:slider

--- loft_homework-1/app/js/main.js
+++ loft_homework-1/app/js/main.js
@@ -1,24 +1,50 @@
-const body = document.body;
-const burger = document.querySelector('.burger__menu');
-const nav = document.querySelector('.list');
-const activeClass = 'is-active';
-
-burger.addEventListener('click', (e) =>{
-  e.preventDefault();
-  toggleClass();
-});
-
-nav.addEventListener('click', (e) => {
-  e.preventDefault();
-  if(e.target.classList.contains('list__link')) {
+if (window.innerWidth <= 768) {
+  const body = document.body;
+  const burger = document.querySelector('.burger__menu');
+  const nav = document.querySelector('.list');
+  const activeClass = 'is-active';
+  
+  burger.addEventListener('click', (e) =>{
+    e.preventDefault();
     toggleClass();
-  }
-});
+  });
+  
+  nav.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(e.target.classList.contains('list__link')) {
+      toggleClass();
+    }
+  });
+  
+  
+  function toggleClass (e) {
+    body.classList.contains(activeClass) ? body.classList.remove(activeClass) :body.classList.add(activeClass); 
+  };
+ };
+
+ $('.price__slider').bxSlider();
+
+ 
 
 
-function toggleClass (e) {
-  body.classList.contains(activeClass) ? body.classList.remove(activeClass) :body.classList.add(activeClass) 
-};
+
+             
+//   const openItem = item => {
+//   const container = item.closest(".team__list-item");
+//   const contentBlock = container.find(".team__name"); // возможно потребует правки
+//   const textBlock = contentBlock.find(".team__text");
+//   const reqHeight = textBlock.height();
+
+//   contentBlock.height(reqHeight);
+//  }
+
+//  $('.team__link').click(e =>{
+//    const $this = $(e.currentTarget);
+
+//    openItem($this);
+//  });
+
+
 
 
 
