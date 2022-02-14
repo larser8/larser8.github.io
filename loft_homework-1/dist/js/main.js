02837991d29540e3e4f8e2ba2fc473c59567f4e7
Denis Beletskiy <denisbeletskiy@Deniss-MacBook-Air.local>
2022-02-14 12:10:46 +0200

    hw5:bug

--- loft_homework-1/dist/js/main.js
+++ loft_homework-1/dist/js/main.js
@@ -177,7 +177,28 @@ $('.team__title').click(e => {
     openItem($this);
   }
   
+});
 
+                            //Open Menu
+
+const mesureWidth = () => {
+  return 500;
+}
+
+const openItem = item => {
+  const hiddenContent = item.find(".products-menu__content");
+  const reqWidth = mesureWidth();
+
+  hiddenContent.width(reqWidth);
+}
+
+$(".products-menu__title").on("click", e => {
+  e.preventDefault();
+
+  const $this = $(e.currentTarget);
+  const item = $this.closest(".products-menu__item");
+
+  openItem(item);
 
 });
 
